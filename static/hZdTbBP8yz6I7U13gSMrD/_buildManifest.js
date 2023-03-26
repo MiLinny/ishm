26b298b52a5268221311ca8abb4c086c4af6d983
Michael Lin <mjl@canva.com>
2023-03-26 23:09:24 +1100

    Updates

---
+++ static/hZdTbBP8yz6I7U13gSMrD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b006b331397fd480.css","static/chunks/pages/index-e1f9360d7db57f38.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
